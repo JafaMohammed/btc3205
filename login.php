e751c23de440d2ddc9ccc35564870751743a426a
Jaafar Mo <JaafarMuhammad@Jaafars-MacBook-Pro.local>
2020-05-03 07:31:39 +0300

    added logout file

--- login.php
+++ login.php
@@ -26,10 +26,6 @@ if(isset($_POST['btn-login']))
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     <script type="text/javascript" src="validate.js"></script>
     <link rel="stylesheet" type="text/css" href="validate.css">
@@ -38,10 +34,10 @@ if(isset($_POST['btn-login']))
     <form method="post" name="login" id="login" action="<?=$_SERVER['PHP_SELF']?>">
         <table align="center">
             <tr>
-                <td><input type="text" name="username" id="uname" required placeholder="Username"></td>
+                <td><input type="text" name="username" placeholder="Username"></td>
             </tr>
             <tr>
-                <td><input type="password" name="password" id="pass" required placeholder="Password"></td>
+                <td><input type="password" name="password"  placeholder="Password"></td>
             </tr>
             <tr>
                 <td><button type="submit" name="btn-login"><strong>LOGIN</strong></button></td>
