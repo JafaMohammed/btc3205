44bbc27e850da9bc5fa0c6892dbd09c2eb792d11
Jaafar Mohammed <MoJaafar@Jaafars-MacBook-Pro.local>
2020-07-12 12:28:35 +0300

    lab2 part 3

--- login.php
+++ login.php
@@ -1,48 +1,51 @@
-<?php
-include_once "user.php";
-include_once "DBConnector.php";
+<?php 
+include_once 'DBConnector.php';
+include_once 'user.php';
 
-$con = new DBConnector();
+$con = new DBConnector;
 
-if(isset($_POST['btn-login']))
-{
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if (isset($_POST['btn-login'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$instance = User::create();
+	$instance->setPassword($password);
+	$instance->setUsername($username);
 
-    if(User::isPasswordCorrect($password,$username))
-    {
-        User::createUserSession($username);
-        echo "complete";
-        header("Location:privatePage.php");
-    }
-    else
-    {
-        header('Location:login.php');
-        echo ""."error";
-    }
+	if ($instance->isPasswordCorrect()) {
+		$instance->login();
+		$con->closeDatabase();
+		$instance->createUserSession();
+	} else {
+		$instance->createFormErrorSessions("Username or password is incorrect");
+		$con->closeDatabase();
+	}
 }
-?>
 
-<!doctype html>
-<html lang="en">
+
+ ?>
+<!DOCTYPE html>
+<html>
 <head>
-    <title>Login</title>
-    <script type="text/javascript" src="validate.js"></script>
-    <link rel="stylesheet" type="text/css" href="validate.css">
+	<title>Title goes here</title>
+	<script type="text/javascript" src="validate.js"></script>
+	<link rel="stylesheet" type="text/css" href="validate.css">
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="timezone.js"></script> -->
 </head>
 <body>
-    <form method="post" name="login" id="login" action="<?=$_SERVER['PHP_SELF']?>">
-        <table align="center">
-            <tr>
-                <td><input type="text" name="username" placeholder="Username"></td>
-            </tr>
-            <tr>
-                <td><input type="password" name="password"  placeholder="Password"></td>
-            </tr>
-            <tr>
-                <td><button type="submit" name="btn-login"><strong>LOGIN</strong></button></td>
-            </tr>
-        </table>
-    </form>
+	<form method="post" name="login" id="login" action="<?=$_SERVER['PHP_SELF']?>">
+    <table align= "center">
+	<tr>
+        <td><input type="text" name="username" placeholder="Username" required/></td>
+    </tr>
+    <tr>
+        <td><input type="text" name="password" placeholder="Password" required/></td>
+    </tr>
+   <tr>
+   <td><button type="submit" name="btn-login"><strong>LOGIN</strong></button></td>
+   </tr>
+</table>
+
+	</form>
 </body>
 </html>
